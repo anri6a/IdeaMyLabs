95edce6a146481c394169e7e00502bf0e1118ef5
anri6a <anri6a@gmail.com>
2017-10-30 15:33:36 +0300

    консольный калькулятор для сложения и вычитания и выхода по "q"

---
+++ src/myLabs/EasyCalc.java
@@ -0,0 +1,42 @@
+package myLabs;
+
+import java.util.Scanner;
+
+/**
+ * Created by 12 on 30.10.2017.
+ * console calculator for addition and subtraction and 'q' like exit
+ */
+public class EasyCalc {
+    public static void main(String args[]) {
+        String in;
+        int res = 0;
+        for (int i = 0; ; i++) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("введите первое число");
+            int a = sc.nextInt();
+            System.out.println("введите второе число");
+            int b = sc.nextInt();
+            System.out.println("1 - сложить");
+            System.out.println("2 - вычесть");
+            System.out.println("q - выйти");
+            in = sc.next();
+            if (in.equals("q"))
+                break;
+            else {
+
+                switch (in) {
+                    case "1":
+                        res = a + b;
+                        break;
+                    case "2":
+                        res = a - b;
+                        break;
+                    default:
+                        System.out.println("неверный выбор");
+                }
+                System.out.println("результат - " + res);
+            }
+
+        }
+    }
+}

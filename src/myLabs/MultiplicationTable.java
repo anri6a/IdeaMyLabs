7071607c345ea0e9ac40a74be0db593ba866da4d
anri6a <anri6a@gmail.com>
2017-11-02 16:44:02 +0300

    вывод таблицы умножения 10х10

---
+++ src/myLabs/MultiplicationTable.java
@@ -0,0 +1,16 @@
+package myLabs;
+
+/**
+ * Created by 12 on 02.11.2017.
+ * print Multiplication Table 10x10
+ */
+public class MultiplicationTable {
+    public static void main(String args[]){
+        for (int i=1; i<10; i++){
+            for (int j=1; j<10; j++){
+                System.out.printf("%4d", i*j);
+            }
+            System.out.println();
+        }
+    }
+}
